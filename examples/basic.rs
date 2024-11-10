c0cc349e451cb2104024234449c04a93bfe7c133
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2024-11-10 09:15:40 -0500

    refactor: combine basic and interpolation examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -20,21 +20,35 @@ fn setup(mut commands: Commands) {
             display: Display::Flex,
             align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
+            flex_direction: FlexDirection::Column,
+            row_gap: Val::Px(20.),
             ..Default::default()
         })
         .with_children(|parent| {
+            // Basic usage of the i18n text and font components
             parent.spawn((
                 // i18n text component with key "hello"
                 I18nText::new("hello"),
                 // Dynamic font component with font family "NotoSans" that auto loads font files based on the set locale
                 I18nFont::new("NotoSans"),
                 // You can still insert a TextFont component though
-                // Just keep in mind that the "font" field will be overridden by the I18nFont component
+                // Keep in mind that the "font" field will be overridden by the I18nFont component
                 TextFont {
                     font_size: 40.0,
                     ..default()
                 },
-                TextLayout::new_with_justify(JustifyText::Center),
+            ));
+
+            // Interpolation example
+            parent.spawn((
+                // You can add as many arguments as you want to the translation
+                I18nText::new("messages.hello")
+                    .with_arg("name", "Bevy User")
+                    // You can also specify the locale for this specific translation
+                    // This overrides the globally set locale
+                    .with_locale("ja"),
+                // Dynamic font component with font family "NotoSans" that auto loads font files based on the set locale
+                I18nFont::new("NotoSans"),
             ));
         });
 }
